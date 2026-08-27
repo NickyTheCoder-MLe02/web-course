7a3884938f244d52a5dc32b412bc0f985b8afb0b
mle <yrinora@gmail.com>
2026-08-27 15:20:02 +0700

    long toi dau don khi nhan ra toi la gay

--- web-course/app.js
+++ web-course/app.js
@@ -6,16 +6,15 @@ console.log(title.textContent);
 let form = document.querySelector("#house-form");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Dừng hành động tải lại trang mặc định
+    event.preventDefault();
 
     let bedrooms = document.querySelector("#bedrooms").value;
 
-    // Kiểm tra nếu dữ liệu trống hoặc giá trị <= 0
     if (bedrooms === "" || Number(bedrooms) <= 0) {
-        let error = document.createElement("p"); // Tạo một thẻ <p> mới
-        error.textContent = "Bedrooms must be a positive number.";
+        let error = document.createElement("p");
+        error.textContent = "Cốc cốc, mở cửa cho anh đê, để cho anh cưa nhá";
         error.style.color = "red";
-        form.appendChild(error); // Gắn thông báo lỗi này vào form
+        form.appendChild(error);
     } else {
         console.log("Form is valid - ready to send.");
     }
